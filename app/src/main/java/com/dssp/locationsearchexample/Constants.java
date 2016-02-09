24f8a8508c812195f427f21ba3ec49267d4549a5
NaikDhanraj <dssp522@gmail.com>
2016-02-10 01:02:18 +0530

    android location search from list android location search autocomplete

---
+++ app/src/main/java/com/dssp/locationsearchexample/Constants.java
@@ -0,0 +1,11 @@
+package com.dssp.locationsearchexample;
+
+/**
+ * Created by dhanrajnaik522 on 2/10/2016.
+ */
+public class Constants {
+
+    public static final String API_NOT_CONNECTED = "Google API not connected";
+    public static final String SOMETHING_WENT_WRONG = "OOPs!!! Something went wrong...";
+    public static String PlacesTag = "Google Places Auto Complete";
+}
